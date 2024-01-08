b7acfb629e0bb1b00ecdc4f520367ffa125aef06
Sean5P <>
2024-01-08 15:38:59 +0000

    Fixed Indentation Issues

--- run.py
+++ run.py
@@ -17,14 +17,13 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-print("Please enter sales data from the last market.")
-print("Data should be six numbers, seperated by commas.")
-print("Example: 10,20,30,40,50,60\n")
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, seperated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-data_str = input("Enter your data here: ")
-
-sales_data = data_str.split(",")
-validate_data(sales_data)
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
 
 
 def validate_data(values):
@@ -33,9 +32,9 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into list,
     or if there aren't exactly 6 values.
     """
-    print(values)
     try:
-         if len(values) != 6:
+        [int(value) for value in values]
+        if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
