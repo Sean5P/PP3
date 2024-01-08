1535ca5b9bbfdcb9b4d23d729f7dd6e597fa8774
Sean5P <>
2024-01-08 15:07:32 +0000

    corrected script typos

--- run.py
+++ run.py
@@ -12,11 +12,11 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
     """
-
 print("Please enter sales data from the last market.")
 print("Data should be six numbers, seperated by commas.")
 print("Example: 10,20,30,40,50,60\n")
@@ -33,19 +33,14 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into list,
     or if there aren't exactly 6 values.
     """
+    print(values)
     try:
-        if len(values) != 6:
+         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        
+
+
 get_sales_data()
-
-
-
-
-
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
