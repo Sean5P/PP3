481f0a4015999ca42c263bc1af0f9683841aa394
Sean Kirwan <Sean@Kirwan.ie>
2024-01-08 23:06:52 +0000

    Update run.py
    Updated Code for Heroku

--- run.py
+++ run.py
@@ -116,3 +116,21 @@ def calculate_stock_data(data):
         new_stock_data.append(round(stock_num))
 
     return new_stock_data
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
